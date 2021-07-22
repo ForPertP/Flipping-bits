0d3f4a51b34290c791492afad9ec41acfae33a7a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-07-23 03:27:22 +0900

    Update Flipping-bits.cpp

--- Flipping-bits.cpp
+++ Flipping-bits.cpp
@@ -1,7 +1,11 @@
 long flippingBits(long n)
 {
+    return UINT_MAX - n;
+}
+
+long flippingBits1(long n)
+{
     long result = ~n;
-    
     return result < 0 ? UINT_MAX + result + 1 : result;
 }
 
