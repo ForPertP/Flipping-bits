9169d8e3309f597319bbcdddeaf5d1afc09d9a94
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-02-15 08:54:26 +0900

    Update Flipping-bits.cpp

--- Flipping-bits.cpp
+++ Flipping-bits.cpp
@@ -1,11 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'flippingBits' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
 long flippingBits(long n)
 {
     return UINT_MAX - n;
 }
 
-long flippingBits1(long n)
+long flippingBits2(long n)
 {
     long result = ~n;
     return result < 0 ? UINT_MAX + result + 1 : result;
 }
-
